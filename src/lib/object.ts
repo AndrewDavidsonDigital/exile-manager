32bbb3c18d56d2a7e3ad29ce6a6143de98750751
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-06-02 12:47:35 +1000

    DX: add object manipultaiton utils

---
+++ src/lib/object.ts
@@ -0,0 +1,22 @@
+/**
+ * Fast and loose variable cloning with no-linking from source variable
+ * 
+ * @description Should only be used for non-functional variables, i.e: `arrays`, `objects`, but not `Sets` or `Maps`
+ * @param from Variable to replicate
+ * @returns a new instance of the object `from`
+ */
+export function _cloneDeep<Type>(from: Type) {
+  return JSON.parse(JSON.stringify(from));
+}
+
+/**
+ * Fast and loose objects content comparator.
+ * 
+ * @description Note: Ignores order of children and checks nothing but children and their contents.
+ * @param x First object to compare with
+ * @param y Second object to compare with
+ * @returns True if all children of `X` are in `Y` and `Y` has no other children
+ */
+export function _isObjectEqual( x: object | undefined, y: object | undefined ):boolean{
+  return JSON.stringify(x) === JSON.stringify(y) 
+}
