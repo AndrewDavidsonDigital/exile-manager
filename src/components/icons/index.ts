7c7ff47a71c620c9ddfbd06f1f8de4d7651155e4
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-06-03 21:25:06 +1000

    UI: Add Icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,46 +1,11 @@
-import IconCommunity from "./IconCommunity.vue";
-import IconDocumentation from "./IconDocumentation.vue";
-import IconEcosystem from "./IconEcosystem.vue";
-import IconSupport from "./IconSupport.vue";
-
-import IconSpeedBG from "./IconSpeedBG.vue";
-import IconSpeedPaused from "./IconSpeedPaused.vue";
-
-import IconSpeedFastest from "./IconSpeedFastest.vue";
-import IconSpeedFast from "./IconSpeedFast.vue";
-import IconSpeedNormal from "./IconSpeedNormal.vue";
-import IconSpeedSlow from "./IconSpeedSlow.vue";
-import IconSpeedSlowest from "./IconSpeedSlowest.vue";
-
-import IconPlus from "./IconPlus.vue";
-import IconMinus from "./IconMinus.vue";
-
-import IconVolume from "./IconVolume.vue";
-import IconVolumeOff from "./IconVolumeOff.vue";
-
+import IconRepeat from "./IconRepeat.vue";
 import IconPlay from "./IconPlay.vue";
 import IconPause from "./IconPause.vue";
 
+
 export {
-  IconPlus,
-  IconMinus,
-  IconCommunity,
-  IconDocumentation,
-  IconEcosystem,
-  IconSupport,
-
-  IconSpeedBG,
-  IconSpeedPaused,
-
-  IconSpeedFastest,
-  IconSpeedFast,
-  IconSpeedNormal,
-  IconSpeedSlow,
-  IconSpeedSlowest,
-
-  IconVolume,
-  IconVolumeOff,
-
   IconPlay,
   IconPause,
+
+  IconRepeat,
 };
