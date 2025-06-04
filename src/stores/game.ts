ceb030a7b3c2f968f25345641e34b2c73d6763a1
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-06-04 13:20:50 +1000

    Version - 0.0.6

--- src/stores/game.ts
+++ src/stores/game.ts
@@ -25,7 +25,7 @@ import { calculateDodgeChance, getAffixValue, getAffixValueRange, resolveAverage
 import { allItemTypes, slotMap, generateItemLevel } from '@/lib/itemUtils';
 
 const LOGGING_PREFIX = '🎮 Game Engine:\t';
-const VERSION_NUMBER = '0.0.5';
+const VERSION_NUMBER = '0.0.6';
 
 const DEFAULT_STATE = <Readonly<IGameEngine>> {
   version: VERSION_NUMBER,
