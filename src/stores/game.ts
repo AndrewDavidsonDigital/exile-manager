0ea29bc61fe9ee2de833e462fadbbd6842493b31
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-06-05 21:09:49 +1000

    Version - 0.0.11

--- src/stores/game.ts
+++ src/stores/game.ts
@@ -26,7 +26,7 @@ import { allItemTypes, slotMap, generateItemLevel, getWeightedItemType, generate
 import { calculateDodgeChance } from '@/lib/combatMechanics';
 
 const LOGGING_PREFIX = '🎮 Game Engine:\t';
-const VERSION_NUMBER = '0.0.10';
+const VERSION_NUMBER = '0.0.11';
 
 const DEFAULT_STATE = <Readonly<IGameEngine>> {
   version: VERSION_NUMBER,
