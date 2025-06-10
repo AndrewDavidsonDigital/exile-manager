f5de5d564b31bd41fb4b47a05bc426b8136023d6
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-06-10 15:31:30 +1000

    UI: Update landing copy

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -14,8 +14,16 @@
       <p>This is a small project (with minimal AI usage)</p>
       <p>To start adventuring navigate to `new-game` or click the button bellow</p>
       <p>
-        Additionally due to the way audio auto-play works you'll need to interact with the audio element in the footer before you hear the glorious Stellaris bgm
-      </p>
+        Additionally due to the way audio auto-play works you'll need to interact with the audio element in the footer before you hear the glorious Stellaris bgm.
+      </p><br>
+      <h3 class="text-left">
+        Notes:
+      </h3>
+      <ul class="text-left list-disc list-inside">
+        <li>Unlike other ARPGs item tiers are meant to be asymmetrical</li>
+        <li>Items are expected to be able to roll multiple Affixes of the same type</li>
+        <li>Affix tier rolls will be banded relative to the item level of the item, both lower and upper</li>
+      </ul>
       <button
         class="size-full"
         @click="() => router.push('/game-state')"
