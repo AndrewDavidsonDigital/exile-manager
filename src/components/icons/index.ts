fab2caf34884458877453a3efc60084e6f426beb
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-06-11 17:11:12 +1000

    Engine: Add new icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -6,7 +6,10 @@ import IconPassiveTree from './IconPassiveTree.vue';
 import IconSkills from './IconSkills.vue';
 import IconWorldSkills from './IconWorldSkills.vue';
 import IconStatIncrease from './IconStatIncrease.vue';
-
+import IconHelm from './IconHelm.vue';
+import IconMap from './IconMap.vue';
+import IconTreasureChest from './IconTreasureChest.vue';
+import IconVillage from './IconVillage.vue';
 
 export {
   IconPlay,
@@ -19,4 +22,9 @@ export {
   IconSkills,
   IconWorldSkills,
   IconStatIncrease,
+
+  IconHelm,
+  IconMap,
+  IconTreasureChest,
+  IconVillage,
 };
