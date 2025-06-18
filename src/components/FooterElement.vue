452019a4a1687e04c4bc76c16b4bd0c58b6adbcd
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-06-18 17:08:50 +1000

    UI: Add custom selection colour

--- src/components/FooterElement.vue
+++ src/components/FooterElement.vue
@@ -34,7 +34,7 @@
   >
     <BgmBar />
     <button 
-      class=" border border-black px-2 rounded-lg bg-amber-700 text-black"
+      class=" border border-black px-2 rounded-lg bg-amber-700 text-black selection:!bg-amber-400/40"
       :class="[
         {'pointer-events-none': adventuringStore.isAdventuring },
       ]"
